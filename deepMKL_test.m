dd28d2fac96df268e57babac3fe39f25e079ec97
Eric V. Strobl <ericvonstrobl@gmail.com>
2014-05-21 11:34:19 -0400

    Create deepMKL_test.m

---
+++ deepMKL_test.m
@@ -0,0 +1,24 @@
+function [pred,acc] = deepMKL_test(xA,y,model,net)
+% Deep Multiple Kernel Learning by Span Bound
+% 
+% Inputs:
+% (1) xA = trainng and testing data matrix, where rows are instances and columns are features
+% (2) y = testing target matrix, where rows are instances
+% (3) model = LIBSVM model
+% (4) net = net parameters
+%
+% Outputs:
+% (1) pred = predictions
+% (2) acc = accuracy
+%
+% Citation: Strobl EV & Visweswaran S. Deep Multiple Kernel Learning.
+% ICMLA, 2013.
+
+dotxA = xA*xA';
+[~,Kf] = computeKernels(dotxA,net.sig,net.w,net.nLayers);
+
+[r,~] = size(dotxA);
+Ks = reshape(Kf(:,net.nLayers),r,r);
+n = net.n;
+Ks = Ks(n+1:end,1:n);
+[pred, acc, ~] = svmpredict(y, [(1:r-n)' Ks], model);
