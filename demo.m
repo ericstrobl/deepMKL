07bc6ce73c60d30d394ebaa054de6cae3b708218
Eric V. Strobl <ericvonstrobl@gmail.com>
2014-05-23 09:21:49 -0400

    Update demo.m

--- demo.m
+++ demo.m
@@ -4,11 +4,11 @@ clc
 [x, Xtest, y, Ytest] = loadGermanDatasetCP();
 
 %one layer
-[model,net] = deepMKL_train(x,y,1);
+[model,net] = deepMKL_train(x,y,1,10,1E-3);
 [pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
 
 %two layer
-[model,net] = deepMKL_train(x,y,2);
+[model,net] = deepMKL_train(x,y,2,10,1E-3);
 [pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
 
 %three layer
