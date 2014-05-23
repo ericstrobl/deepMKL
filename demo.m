1d39853381b4e9e59903ac24a7fc8835413d8b48
Eric V. Strobl <ericvonstrobl@gmail.com>
2014-05-23 12:45:49 -0400

    Update demo.m

--- demo.m
+++ demo.m
@@ -8,7 +8,7 @@ clc
 [pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
 
 %two layer
-[model,net] = deepMKL_train(x,y,2,10,1E-3);
+[model,net] = deepMKL_train(x,y,2,10,1E-5);
 [pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
 
 %three layer
