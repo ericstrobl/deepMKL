f296f047a8fa9cb8f5ac95995a75c4379fd0e003
Eric V. Strobl <ericvonstrobl@gmail.com>
2014-05-25 11:57:01 -0400

    Create demo.m

---
+++ demo.m
@@ -0,0 +1,53 @@
+clear all
+clc
+
+% Trains 1,2 and 3 layer models on three example datasets
+
+%% GERMAN
+disp('GERMAN')
+
+[x, Xtest, y, Ytest] = loadGermanDataset();
+
+%one layer
+[model,net] = deepMKL_train(x,y,1,1E-4);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%two layer
+[model,net] = deepMKL_train(x,y,2,1E-5);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%three layer
+[model,net] = deepMKL_train(x,y,3,1E-5);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%% GLASS2
+disp('GLASS2')
+[x, Xtest, y, Ytest] = loadGlass2Dataset();
+ 
+%one layer
+[model,net] = deepMKL_train(x,y,1,1E-2);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%two layer
+[model,net] = deepMKL_train(x,y,2,1E-2);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%three layer
+[model,net] = deepMKL_train(x,y,3,1E-2);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%% AUSTRALIAN
+disp('AUSTRALIAN')
+[x, Xtest, y, Ytest] = loadAustralianDataset();
+ 
+%one layer
+[model,net] = deepMKL_train(x,y,1,1E-2);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%two layer
+[model,net] = deepMKL_train(x,y,2,1E-3);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
+
+%three layer
+[model,net] = deepMKL_train(x,y,3,1E-3);
+[pred,acc] = deepMKL_test([x;Xtest],Ytest,model,net);
